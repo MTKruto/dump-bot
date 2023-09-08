f42a8246670737493bfa2f201ad82eed981e3ff4
Roj <ez@roj.im>
2023-09-08 20:23:33 +0300

    Initial functionality

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Client } from "mtkruto/mod.ts";
+import { ChatType, Client, types } from "mtkruto/mod.ts";
 import { StorageDenoKV } from "mtkruto/storage/1_storage_deno_kv.ts";
 import env from "./env.ts";
 
@@ -12,3 +12,31 @@ client.on("authorizationState", async ({ authorizationState: { authorized } }) =
 });
 
 await client.start(env.BOT_TOKEN);
+
+client.use(async (update, next) => {
+  const chat = (update.callbackQuery?.message ?? update.message ?? update.editedMessage)?.chat;
+  if (chat && chat.type == ChatType.Private) {
+    try {
+      await next();
+    } catch (err) {
+      if (err instanceof types.RPCError && err.errorMessage.startsWith("FLOOD_WAIT_")) {
+        const duration = Number(err.errorMessage.replace("FLOOD_WAIT_", ""));
+        console.log("Sleeping for", duration, "seconds...");
+        await new Promise((r) => setTimeout(r, duration * 1000));
+        try {
+          await next();
+        } catch (err) {
+          console.error(err);
+        }
+      } else {
+        console.error(err);
+      }
+    }
+  }
+});
+
+client.use(async (update) => {
+  const chat = (update.callbackQuery?.message ?? update.message ?? update.editedMessage)?.chat!;
+  const text = JSON.stringify(update, null, 2);
+  await client.sendMessage(chat.id, text, { entities: [{ type: "code", offset: 0, length: text.length }] });
+});
